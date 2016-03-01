3f812ddcc2fe7764d38642718d497ad54b5d4614
likex <934249626@qq.com>
2016-03-01 23:46:25 +0800

    部署修改

--- config.py
+++ config.py
@@ -14,7 +14,12 @@ class DevelopmentConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir,'data-dev.sqlite')
 
+class ProductionConfig(Config):
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir,'data.sqlite')
+
 config = {
 	'development':DevelopmentConfig,
+	'production':ProductionConfig,
 	'default':DevelopmentConfig
 }
