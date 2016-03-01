304dee146bfa2c31633ec8e982a00e561cd5236f
likex <934249626@qq.com>
2016-03-02 00:08:08 +0800

    bushu2

--- config.py
+++ config.py
@@ -21,5 +21,5 @@ class ProductionConfig(Config):
 config = {
 	'development':DevelopmentConfig,
 	'production':ProductionConfig,
-	'default':DevelopmentConfig
+	'default':ProductionConfig
 }
--- manage.py
+++ manage.py
@@ -9,6 +9,12 @@ app = create_app('default')
 manager =  Manager(app)
 migrate = Migrate(app,db)
 
+@manager.command
+def deploy():
+	from flask.ext.migrate import upgrade
+	from app.models import User
+	upgrade()
+
 def make_shell_context():
 	return dict(app=app,db=db,User=User)
 manager.add_command("shell",Shell(make_context=make_shell_context))
